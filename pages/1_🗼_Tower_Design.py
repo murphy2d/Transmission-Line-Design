01692882433faa2d19d8242b9d593fade776a60a
murphy2d <115802174+murphy2d@users.noreply.github.com>
2026-02-15 01:18:18 +0545

    Update 1_🗼_Tower_Design.py

--- pages/1_🗼_Tower_Design.py
+++ pages/1_🗼_Tower_Design.py
@@ -58,7 +58,7 @@ col1, col2 = st.columns(2, border= True)
 
 with col1:
     st.subheader("MF Limit Chart 📊")
-    mf_limit = pd.read_csv(r"data\mfLimit.csv")
+    mf_limit = pd.read_csv(r"data/mfLimit.csv")
     mf_limit.plot(x="Length", y="mf_Limit", kind="line", title="MF Limit vs Length", xlabel="Length (km)", ylabel="MF Limit")
     st.line_chart(mf_limit, x="Length", y="mf_Limit", x_label="Length (km)", y_label="MF Limit", width="stretch")
 
@@ -68,3 +68,4 @@ with col2:
     mf_limit_value = mflimit(length)
 
     st.write(f"***Mf Limit ({length}) km  :*** {mf_limit_value:.3f}")
+
