fc9983c641460033e985a1e4356da55a633b5e09
murphy2d <115802174+murphy2d@users.noreply.github.com>
2026-02-15 18:35:37 +0545

    Add files via upload

---
+++ pages/1_⚙️_Transmission_Line_Parameters.py
@@ -0,0 +1,141 @@
+import streamlit as st
+import pandas as pd
+from utils.towerCalc import economic_voltage_level_calcuation,economic_voltage_level, mflimit, sil_calculation, mf_i_calculation
+
+#input design parameters
+
+# --- INITIALIZATION ---
+if "p_val" not in st.session_state:
+    st.session_state.p_val = 0
+if "l_val" not in st.session_state:
+    st.session_state.l_val = 0
+
+# --- PERSISTENCE LOGIC ---
+def sync_state():
+    st.session_state.p_val = st.session_state._p
+    st.session_state.l_val = st.session_state._l
+
+st.header(":orange[Design Parameters] 🗼")
+
+global power, length, V, Nc
+
+#input parameters for transmission line design
+
+col1, col2 = st.columns(2, border= True)
+
+with col1:
+    st.subheader("Transmitted Power (MW)")
+    power = st.number_input("Enter the power to be transmitted:", value=st.session_state.p_val, key="_p", on_change=sync_state)
+    st.write(f"Power (P) = {power} MW")
+
+with col2:
+    st.subheader("Transmission Length (km)")
+    length = st.number_input("Enter transmission line length:", value=st.session_state.l_val, key="_l", on_change=sync_state)
+    st.write(f"Length (L) = {length} km")
+
+st.space(size="medium")
+
+#economic voltage level
+
+st.subheader(":orange[Economic Voltage Level] 💰")
+
+#economic voltage formulae
+
+st.latex(r'''V_{eco} = 5.5\cdot \sqrt{\frac{L}{1.6} + \frac{P \cdot 1000}{N_c \cdot cos(\phi) \cdot 150}} \space kV''')
+
+#economic voltage level calculation
+
+col1, col2 = st.columns(2, border= True)
+
+with col1:
+    st.subheader("For Nc = 1")
+    st.latex(fr"""V_{{eco}} = 5.5 \cdot \sqrt{{\frac{{{length}}}{{1.6}} + \frac{{{power} \cdot 1000}}{{{1} \cdot {0.96} \cdot 150}}}}\; kV""")
+    V_eco_1 = economic_voltage_level_calcuation(power, length, 1)
+    st.latex(f"V_{{eco1}} = {V_eco_1:.2f} kV")
+    V1 = economic_voltage_level(V_eco_1)
+    st.latex(f"V_{{1}} = {V1} kV")
+    
+with col2:
+    st.subheader("For Nc = 2")
+    st.latex(fr"""V_{{eco}} = 5.5 \cdot \sqrt{{\frac{{{length}}}{{1.6}} + \frac{{{power} \cdot 1000}}{{{2} \cdot {0.96} \cdot 150}}}}\; kV""")
+    V_eco_2 = economic_voltage_level_calcuation(power, length, 2)
+    st.latex(f"V_{{eco2}} = {V_eco_2:.2f} kV")
+    V2 = economic_voltage_level(V_eco_2)
+    st.latex(f"V_{{2}} = {V2} kV")
+
+
+#mf limit
+col1, col2 = st.columns(2, border= True)
+
+with col1:
+    st.subheader("MF Limit Chart 📊")
+    mf_limit = pd.read_csv("TnDProject/data/mfLimit.csv")
+    mf_limit.plot(x="Length", y="mf_Limit", kind="line", title="MF Limit vs Length", xlabel="Length (km)", ylabel="MF Limit")
+    st.line_chart(mf_limit, x="Length", y="mf_Limit", x_label="Length (km)", y_label="MF Limit", width="stretch")
+
+with col2:
+    st.subheader("MF Limit Data 📊")
+    st.dataframe(mf_limit, width="stretch")
+    mf_limit_value = mflimit(length)
+    st.write(f"***Mf Limit ({length} km)  :*** {mf_limit_value:.3f}")
+
+#choice of voltage level and number of circuit using mf limit by figuring out SIL for both circuit options and comparing with mf limit value 
+
+col1, col2 , col3= st.columns(3, border= True)
+
+with col1:
+    st.subheader("SIL Calculation")
+    st.latex(r'''SIL = \frac{V^2}{Z_{o}} \space kV/km''')
+    SIL_1 = sil_calculation(V1, 400)
+    st.latex(f"SIL_{{1}} = \\frac{{{V1}^2}}{{400}} = {SIL_1:.3f} \\space kV/ohm")
+    SIL_2 = sil_calculation(V2, 200)
+    st.latex(f"SIL_{{2}} = \\frac{{{V2}^2}}{{200}} = {SIL_2:.3f} \\space kV/ohm")
+
+with col2:
+    st.subheader("mfᵢ Calculation")
+    st.latex(r'''mf_i = \frac{P}{SILᵢ}''')
+    mf_i_1 = mf_i_calculation(power, SIL_1)
+    st.latex(f"mf_{{1}} = \\frac{{{power}}}{{{SIL_1:.3f}}} = {mf_i_1:.3f}")
+    mf_i_2 = mf_i_calculation(power, SIL_2)
+    st.latex(f"mf_{{2}} = \\frac{{{power}}}{{{SIL_2:.3f}}} = {mf_i_2:.3f}")
+
+with col3:
+    st.subheader("Design Decision")
+    if mf_i_1 > mf_limit_value and mf_i_2 < mf_limit_value:
+        st.write(f"Since, **mf₁ > mf ₗᵢₘᵢₜ** and **mf₂ < mf ₗᵢₘᵢₜ**, we choose V₂ and Nc = 2")
+        V = V2
+        Nc = 2
+    elif mf_i_1 < mf_limit_value and mf_i_2 > mf_limit_value:
+        st.write(f"Since, **mf₁ < mf ₗᵢₘᵢₜ** and **mf₂ > mf ₗᵢₘᵢₜ**, we choose V₁ and Nc = 1")
+        V = V1
+        Nc = 1
+    elif mf_i_1 and mf_i_2 < mf_limit_value:
+        if V1 == V2:
+            st.write("Since, mf₁ and mf₂ < mf ₗᵢₘᵢₜ and V₁=V₂ ,we choose V₁ and Nc = 1")
+            V = V1
+            Nc = 1
+        elif V1 != V2:
+            PM1 = mf_limit_value*SIL_1-power
+            PM2 = mf_limit_value*SIL_2-power
+            st.write(f"PM₁ = mfₗᵢₘᵢₜ X SIL₁ - P = {mf_limit_value:.3f} * {SIL_1:.3f} - {power} = {PM1:.3f}")
+            st.write(f"PM₂ = mfₗᵢₘᵢₜ X SIL₂ - P = {mf_limit_value:.3f} * {SIL_2:.3f} - {power} = {PM2:.3f}")
+            if PM1 > PM2:
+                st.write(f"Since, mf₁ and mf₂ < mf ₗᵢₘᵢₜ and PM₁ > PM₂ ,we choose V₂ and Nc = 2")
+                V = V2
+                Nc = 2
+            elif PM2 > PM1:
+                st.write(f"Since, mf₁ and mf₂ < mf ₗᵢₘᵢₜ and PM₂ > PM₁ ,we choose V₁ and Nc = 1")
+                V = V1
+                Nc = 1
+        
+    else:
+        st.write("Neither option is feasible based on the MF limit. You may need to reconsider your design parameters and increase the voltage level. ")
+
+    st.write(f"**Chosen Voltage Level (V) :** {V} kV")
+    st.write(f"**Chosen Number of Circuits (Nc) :** {Nc}")
+    
+    #set voltage level and number of circuits in session state for use in other pages
+    st.session_state.V = V
+    st.session_state.Nc = Nc
+
+
---
+++ pages/2_🗼_Transmission_Tower_Calculation.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import pandas as pd
+from utils.towerCalc import air_clearance_calculation, crossarm_length_calculation, insulated_string_length_calculation, distance_between_conductors_calculation, distance_between_earth_wire_and_top_conductor_calculation, distance_of_earth_wire_from_top_most_cross_arm_calculation
+
+
+#global variables for voltage level and number of circuits
+V = st.session_state.V
+Nc = st.session_state.Nc
+
+#earth wire design
+
+st.header(":orange[Earth Wire Selection] 🌩️")
+
+col1, col2 = st.columns(2, border= True)
+
+with col1:
+    st.subheader("Earth Wire Selection based on Voltage Level")
+    earth_wire_options = pd.DataFrame({
+        "Voltage Level (kV)": ["Low Voltage (upto 66 kV)", "Medium Voltage (132 - 220 kV)", "High Voltage (> 220 kV)"],
+        "Recommended Earth Wire": ["Single Earth Wire", "Single Earth Wire for Nc =1 and Double Earth Wire for Nc = 2", "Double Earth Wire"]})
+    st.dataframe(earth_wire_options)
+
+with col2:
+    st.subheader("Recommended Earth Wire")
+    if V <= 66:
+        st.write("Based on the chosen voltage level, a \n\n**Single Earth Wire** is recommended.")
+    elif 132 <= V <= 220:
+        if Nc == 1:
+            st.write("Based on the chosen voltage level and number of circuits, a \n\n**Single Earth Wire** is recommended.")
+        elif Nc == 2:
+            st.write("Based on the chosen voltage level and number of circuits, \n\n**Double Earth Wire** is recommended.")
+    elif V > 220:
+        st.write("Based on the chosen voltage level, \n\n**Double Earth Wire** is recommended.")
+
+st.space(size="medium")
+
+#transmission tower design
+
+# --- INITIALIZATION ---
+if "swing_angle" not in st.session_state:
+    st.session_state.swing_angle = 0
+
+# --- PERSISTENCE LOGIC ---
+def sync_state():
+    st.session_state.swing_angle = st.session_state.swing
+
+st.header(":orange[Transmission Tower Design] 🏗️")
+
+col1, col2 = st.columns(2, border= True)
+
+with col1:
+    if Nc == 1:
+        st.image("TnDProject/data/TransmissionLineNc1.png", caption="Transmission Tower for Single Circuit", width="stretch")
+    elif Nc == 2:
+        st.image("TnDProject/data/TransmissionLineNc2.png", caption="Transmission Tower for Double Circuit", width="stretch")
+
+with col2:
+    st.subheader("Tower Parameter Calculation")
+    swing_angle = st.number_input("Enter the swing angle (degrees):", value=st.session_state.swing_angle, key="swing", on_change=sync_state)
+    st.write(f"Swing Angle (θ) = {swing_angle} degrees")
+
+    air_clearance = air_clearance_calculation(V)
+    st.latex(fr"""Air \, Clearance \, (a) = 1 \cdot {V} \cdot 1.1 \cdot \left( \frac{{\sqrt{2}}}{{\sqrt{3}}} \right) + 30 = {air_clearance:.2f} \; cm""")
+    
+    crossarm_length = crossarm_length_calculation(swing_angle, air_clearance)
+    st.latex(fr"""Crossarm \, Length \, (cl) = {air_clearance:.2f} \cdot \left( 1 + \tan\left( {swing_angle}^o \right) \right) = {crossarm_length:.2f} \; cm""")
+    
+    insulated_string_length = insulated_string_length_calculation(swing_angle, air_clearance)
+    st.latex(fr"""Insulated \, String \, Length \, (l) = \frac{{{air_clearance:.2f}}}{{\cos\left( {swing_angle}^o \right)}} = {insulated_string_length:.2f} \; cm""")
+    
+    distance_between_conductors = distance_between_conductors_calculation(insulated_string_length, crossarm_length, air_clearance)
+    st.latex(fr"""Distance \, between \, Conductors \, (y) = \frac{{{insulated_string_length:.2f} + {air_clearance:.2f}}}{{\sqrt{{1 - 0.3^2 \cdot \left( \frac{{{insulated_string_length:.2f} + {air_clearance:.2f}}}{{{crossarm_length:.2f}}} \right)^2}}}} = {distance_between_conductors:.2f} \; cm""")
+
+    tower_width = 1.5 * air_clearance
+    st.latex(fr"""Tower \, Width \, (b) = 1.5 \cdot {air_clearance:.2f} = {tower_width:.2f} \; cm""")
+
+    ROW = 2*crossarm_length + tower_width
+    st.latex(fr"""Right \, of \, Way \, (ROW) = 2 \cdot {crossarm_length:.2f} + {tower_width:.2f} = {ROW:.2f} \; cm""")
+
+    d_dash = distance_between_earth_wire_and_top_conductor_calculation(crossarm_length, tower_width, Nc)  
+    if Nc == 1:
+        st.latex(fr"""Distance \, between \, Earth \, Wire \, and \, Top \, Conductor \, (d') = \sqrt{3} \cdot \left( {crossarm_length:.2f} - \frac{{{tower_width:.2f}}}{2} \right) = {d_dash:.2f} \; cm""") 
+    elif Nc == 2:
+        st.latex(fr"""Distance \, between \, Earth \, Wire \, and \, Top \, Conductor \, (d') = \sqrt{3} \cdot {crossarm_length:.2f} = {d_dash:.2f} \; cm """)
+
+    d = distance_of_earth_wire_from_top_most_cross_arm_calculation(d_dash, insulated_string_length)
+    st.latex(fr"""d = d' - l = {d_dash:.2f} - {insulated_string_length:.2f} = {d:.2f} \; cm""")
